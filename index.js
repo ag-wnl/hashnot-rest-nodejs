4c3325be4114b1ccb07763a02782b25318d63039
ag-wnl <agnideep22@gmail.com>
2023-09-02 13:38:56 +0530

    user authentication to client

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 //middlewares:
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true)
+    next()
+})
 app.use(express.json());   //to send json responses
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+}));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
