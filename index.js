286bb2a557f7c536bfce23ce950804490c1694a8
ag-wnl <agnideep22@gmail.com>
2024-01-23 15:38:48 +0530

    feat: Web sockets for realtime chat setup

--- index.js
+++ index.js
@@ -12,14 +12,20 @@ import showRequestRoutes from "./routes/showRequest.js"
 import cookieParser from "cookie-parser";   
 import cors from "cors";
 
-const app = express();
+import http from "http";
+import { WebSocketServer } from "ws"; 
 
-//middlewares:
+const app = express();
+const server = http.createServer(app);
+const wss =  new WebSocketServer({ server });
+
+//middlewares, CORS stuff:
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 app.use(express.json()); //to send json responses
+
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -39,6 +45,35 @@ app.use("/api/urlprev", LinkPreviews);
 app.use("/api/hackathon", hackathonRoutes);
 app.use("/api/showrequest", showRequestRoutes);
 
+wss.on("headers", (headers, req) => {
+  cors()(req, null, () => {
+    headers.push("Access-Control-Allow-Credentials: true");
+    headers.push("Access-Control-Allow-Origin: http://localhost:3000");
+  })
+})
+
+wss.on("connection", (ws) => {
+  console.log("someone connected");
+
+  ws.on("message", (message) => {
+    console.log("recieved: ", message);
+    ws.send(`Hi, here you go: ${message}`);
+  });
+
+  ws.on("close", () => {
+    console.log("connection closed"); 
+  });
+
+  ws.on("error", (error) => {
+    console.error("WebSocket error:", error);
+  });
+});
+
 app.listen(8800, () => {
   console.log("API Active!");
 });
+
+const serverPort = 6565;
+server.listen(serverPort, () => {
+  console.log(`Web socket listening on port: ${serverPort}`);
+})
